f00fa21d8982c46bd5491ea3daeb9abdfa1452cd
nate giraldi <nate@teachable.com>
2019-07-19 14:10:36 -0400

    un-lock package dependencies

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ setup(name="tap-zendesk-chat",
       classifiers=["Programming Language :: Python :: 3 :: Only"],
       py_modules=["tap_zendesk_chat"],
       install_requires=[
-          "python-dateutil==2.6.0",  # because of singer-python issue
-          "pendulum==1.2.0",  # because of singer-python issue
-          "singer-python==3.5.0",
-          "requests==2.20.0",
+          "python-dateutil>=2.6.0",  # because of singer-python issue
+          "pendulum>=1.2.0",  # because of singer-python issue
+          "singer-python>=3.5.0",
+          "requests>=2.20.0",
       ],
       entry_points="""
           [console_scripts]
